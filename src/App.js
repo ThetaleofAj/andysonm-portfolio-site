164a0e5e28e66e0a3c6f5f0f4d3295267beb436f
whenceIT <whenceit@gmail.com>
2023-07-26 21:29:36 +0200

    .

--- src/App.js
+++ src/App.js
@@ -13,23 +13,23 @@ function App() {
   <div className="flex gap-16 mb-10">
     <div className="flex flex-col">
     <p class='font-bold'>Follow me on</p>
-    <a href="https://github.com/thetaleofAj" class='text-center underline' target="_blank">Github</a>
-    <a href="https://twitter.com/_andysonm_" class='text-center underline' target="_blank">Twitter</a>
-    <a href="https://www.linkedin.com/in/andyson-mupeta-339778200/" class='text-center underline' target="_blank">LinkedIn</a>
+    <a href="https://github.com/thetaleofAj" class='text-center underline' target="_blank" rel="noreferrer noopener">Github</a>
+    <a href="https://twitter.com/_andysonm_" class='text-center underline' target="_blank" rel="noreferrer noopener">Twitter</a>
+    <a href="https://www.linkedin.com/in/andyson-mupeta-339778200/" class='text-center underline' target="_blank" rel="noreferrer noopener">LinkedIn</a>
     </div>
 
     <div className="flex flex-col">
     <h3 class='font-bold'>Read my blogs on</h3>
-    <a href="https://andysonm.hashnode.dev/" class='text-center underline' target="_blank">Hashnode</a>
-    <a href="https://medium.com/@mupeta.andyson" class='text-center underline' target="_blank">Medium</a>
+    <a href="https://andysonm.hashnode.dev/" class='text-center underline' target="_blank" rel="noreferrer noopener">Hashnode</a>
+    <a href="https://medium.com/@mupeta.andyson" class='text-center underline' target="_blank" rel="noreferrer noopener">Medium</a>
       </div>
   </div>
   <div className="flex flex-col mb-10">
   <p class='font-bold text-center'>Check out my projects(sorted from most recent)</p>
-  <a href="https://course-assist.com/" class='text-center underline' target="_blank">Course Assist</a>
-  <a href="https://github.com/ThetaleofAj/unza-smart-car-parking-system" class='text-center underline' target="_blank">UNZA smart car parking system</a>
+  <a href="https://course-assist.com/" class='text-center underline' target="_blank" rel="noreferrer noopener">Course Assist</a>
+  <a href="https://github.com/ThetaleofAj/unza-smart-car-parking-system" class='text-center underline' target="_blank" rel="noreferrer noopener">UNZA smart car parking system</a>
   </div>
-  <p class="text-center" >Got questions? Drop me an email at <a href="mailto: andysonmupeta2@gmail.com" class='text-center underline' target="_blank"><span class="underline">andysonmupeta2@gmail.com</span></a></p>
+  <p class="text-center" >Got questions? Drop me an email at <a href="mailto: andysonmupeta2@gmail.com" class='text-center underline' target="_blank" rel="noreferrer noopener"><span class="underline">andysonmupeta2@gmail.com</span></a></p>
 </div>
    </section>
    </>
